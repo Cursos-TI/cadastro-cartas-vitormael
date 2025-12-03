c0920ab0f3bc5795346026678f4fe892dbee8619
vitormael <emanoel.sclx@gmail.com>
2025-12-03 16:08:04 +0000

    vai

---
+++ teste.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef struct {
+    char codigo[4];            // Ex: A01
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+} Carta;
+
+int main (){
+
+
+
+
+
+
+
+    
+}

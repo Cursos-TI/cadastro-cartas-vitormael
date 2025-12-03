5488134ce656bb84f53f15b517f6caba1cbe6255
vitormael <emanoel.sclx@gmail.com>
2025-12-03 15:22:07 +0000

    02 esttrutura

---
+++ supertrunfo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+typedef struct { // As informações presentes na carta //
+
+char codigo[04]; // ex: (A01) mas ele usa um caracter invisivel \0 //
+int pontos_turisticos, população;
+float area, pib;
+
+} cartas;
+
+int main (){
+
+  Carta cartas[2]; // Quatidade de cartas A01, A02//
+
+  printf ("=== Surper Trunfu - cadastro de cartas ===\n");
+
+
+
+
+
+
+
+
+
+return 0 ;
+
+}
